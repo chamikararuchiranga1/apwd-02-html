0cd77f97473dce7f1653d1fdc83388226e4c49c0
chami <chamikararuchiranga1@gmail.com>
2023-07-16 10:06:30 +0530

    global variable and local variable

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,19 @@
 console.log("external java script");
 
-// types 3
+// variable types 3
 // 1. var
 // 2. let
 // 3. const
+
+
+// global variable and local variable
+
+var name = "Chamikara"; // Global Variable
+Check()
+function Check(){
+    var name = "Ruchiranga" // Local variable
+    document.write(name)
+}
+document.write(name);
 
 
