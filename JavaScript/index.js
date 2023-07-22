366983aaf6984b8daa83c195429fd6f8fe8bb073
chami <chamikararuchiranga1@gmail.com>
2023-07-22 09:09:18 +0530

    var, let, const variables

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -8,12 +8,63 @@ console.log("external java script");
 
 // global variable and local variable
 
-var name = "Chamikara"; // Global Variable
-Check()
-function Check(){
-    var name = "Ruchiranga" // Local variable
-    document.write(name)
+// var name = "Chamikara"; // Global Variable
+// Check()
+// function Check(){
+//     var name = "Ruchiranga" // Local variable
+//     document.write(name)
+// }
+// document.write(name);
+
+// Let
+let t;
+let x = 10;
+// let x = 12;
+
+{
+    x = 12;
+    console.log(x)
 }
-document.write(name);
+
+console.log(x)
+console.log("---------------------------------------")
+
+// Var
+var y = 10;
+var y = 10;
+var y = 10;
+y = 23
+y = 23
+y = 23
+
+{
+    y = 30;
+    var w = 20;
+}
+
+console.log(w)
+console.log(y)
+console.log("----------------------------------------")
+
+// Const
+
+const d = 10;
+// const d = 19; ===>error
+// d = 12;  ====> error
+console.log(d);
+
+{
+    console.log(d);
+    // d = 34;
+    // const d = 34; ===> error
+    const g = 78;
+    console.log(g);
+}
+// console.log(g) ===> error
+console.log("----------------------------------------");
+
+
+
+
 
 
