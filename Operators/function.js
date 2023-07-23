803403439faffd9a94d49bfadc247449c9ff1bae
chami <chamikararuchiranga1@gmail.com>
2023-07-23 10:21:12 +0530

    flow controllers

--- Operators/function.js
+++ Operators/function.js
@@ -17,17 +17,18 @@ console.log(result);
 
 // const result1 = maths2023(20, 8);
 console.log(maths2023(10, 8));
+
 // maths2023(2, 5);
 
-function PrintOne(){
+function PrintOne() {
     console.log("Go to Home...!");
 }
 
-function PrintTwo(){
+function PrintTwo() {
     console.log("Go to School..!");
 }
 
-function maths(one, two){
+function maths(one, two) {
     // let one = 180;
     // let two = 200;
     let total = one + two;
@@ -35,8 +36,77 @@ function maths(one, two){
 }
 
 // return function
-function maths2023(one, two){
+function maths2023(one, two) {
     let total = one * two
     return total;
 }
 
+// Flow Controllers
+// If
+const marks = 76;
+mathsTotal = 96;
+if (30 > marks) {
+    console.log("Exam fail...");
+}
+
+// If else
+if (50 < marks) {
+    console.log("Exam passed...")
+} else {
+    console.log("Exam fail...")
+}
+
+// else if
+if (75 <= marks) {
+    console.log("Grade : A");
+} else if (65 <= marks) {
+    console.log("Grade : B");
+} else if (55 <= marks) {
+    console.log("Grade : C");
+} else {
+    console.log("Grade : Fail..");
+}
+
+// Nexted if
+if (75 <= marks) {
+    console.log("Exam passed...");
+    if (mathsTotal > 50) {
+        console.log("pass the Maths...")
+    } else {
+        console.log("Fail the Maths...")
+    }
+} else {
+    if (mathsTotal > 50) {
+        console.log("Maths fail..!")
+    }
+}
+
+// Switch
+switch (marks) {
+    case 76:
+        console.log("Grade : A");
+        break;
+    case 65:
+        console.log("Grade : B");
+        break;
+    case 55:
+        console.log("Grade : C");
+        break;
+    default:
+        console.log("fail..")
+}
+
+getDay("Sunday")
+
+function getDay(val){
+    switch (val){
+        case "Sunday":
+            console.log("Sunday");
+            break;
+        case 2:
+            console.log("Monday");
+            break;
+        default:
+            console.log("not day")
+    }
+}
