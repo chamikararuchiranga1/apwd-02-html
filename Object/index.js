ff9b3060dd53d8746d4c442f899e2e17652f5d19
chami <chamikararuchiranga1@gmail.com>
2023-07-30 10:17:07 +0530

    object

---
+++ Object/index.js
@@ -0,0 +1,14 @@
+console.log("run")
+
+let person = {
+    firstName: "Chamikara",
+    lastName: "Ruchiranga",
+    eyeColor: "blue",
+    run: function (){
+        alert("run");
+    }
+}
+
+console.log(person.firstName+" "+person.lastName);
+person.run();
+
